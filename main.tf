fc097d3c8dc9eb8549dd3ba8d4097fa5a25ec595
Namuna Maharjan <119643216+NamunaGIT@users.noreply.github.com>
2023-03-18 14:56:39 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "docker_server" {
+  ami             = "ami-060d3509162bcc386"
+  instance_type   = "t2.micro"
+  key_name = "namuna
+  security_groups = ["default"]
+  user_data = file("docker-compose.yml
+
+  tags = {
+    Name = "docker_server-terraform"
+  }
+}
